6f191b5d5942e01aef13510f39f7da98c78f78a3
manishpatel0 <mp0000091@gmail.com>
2022-11-11 11:01:02 +0530

    DSA--

---
+++ DSA/README.md
@@ -0,0 +1,4 @@
+#DSA
+DSA - Data Structes Algorithm
+
+Array  Graph  Linkedlist  List  Queue  Stack Heap 
--- usr/C_OOP/ex_00.c
+++ usr/C_OOP/ex_00.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
+
 int main(int argc, char const *argv[])
 {
 	printf("Hello to \n");
+	printf("Argc is %d ", argc);
+	//int size = sizeof(argv/argv[0]);
 	return 0;
 }
