78ca21ba83a5a85bc4837601f54a4d38906793f5
manishpatel0 <mp0000091@gmail.com>
2022-11-23 09:59:38 +0530

    Create first dart console

---
+++ createdart/bin/createdart.dart
@@ -0,0 +1,5 @@
+import 'package:createdart/createdart.dart' as createdart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${createdart.calculate()}!');
+}
---
+++ createdart/test/createdart_test.dart
@@ -0,0 +1,8 @@
+import 'package:createdart/createdart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
