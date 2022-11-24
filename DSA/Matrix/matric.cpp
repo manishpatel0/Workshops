36d6e9733949188b88c40aeb4ee17c59a6e12969
manishpatel0 <mp0000091@gmail.com>
2022-11-24 10:29:16 +0530

    Matrix in DSA

--- DSA/Matrix/matric.cpp
+++ DSA/Matrix/matric.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 using namespace std;
-
+// with out run time define the size in matric 
 int main(int argc, char const *argv[])
 {
-	cout << "Hello"<< endl;
+	cout << "Hello Matric" << endl;
+	int size,n;
+	cin >> size;
+	size = n;
+
+	int **arr = new int*[n];
 	return 0;
 }
